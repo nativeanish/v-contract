5d41164a318bcc5f95dd402e4209a7f9d5f1660a
Anish Gupta <nativeanish@gmail.com>
2023-11-04 05:32:53 +0545

    upload

--- action/read/get_playlist.ts
+++ action/read/get_playlist.ts
@@ -9,10 +9,14 @@ import { Action, State, _q_return } from "../types";
  * @return {*}  {_q_return}
  */
 export function get_playlist(state: State, action: Action): _q_return {
-    const playlist = state.playlist.find((e) => e.id === action.input.id)
-    if (playlist?.id?.length) {
-        return { result: { success: true, data: playlist } }
+    if (action.input.id?.length) {
+        const playlist = state.playlist.find((e) => e.id === action.input.id)
+        if (playlist?.id?.length) {
+            return { result: { success: true, data: playlist } }
+        } else {
+            return { result: { success: false, data: "No Playlist Found" } }
+        }
     } else {
-        return { result: { success: false, data: "No Playlist Found" } }
+        return { result: { success: false, data: "Playlist Id is not define" } }
     }
 }
--- action/read/get_video.ts
+++ action/read/get_video.ts
@@ -9,10 +9,14 @@ import { Action, State, _q_return } from "../types";
  * @return {*}  {_q_return}
  */
 export function get_video(state: State, action: Action): _q_return {
-    const video = state.video.find((e) => e.id === action.input.id)
-    if (video?.id?.length) {
-        return { result: { success: true, data: video } }
+    if (action.input.id?.length) {
+        const video = state.video.find((e) => e.id === action.input.id)
+        if (video?.id?.length) {
+            return { result: { success: true, data: video } }
+        } else {
+            return { result: { success: false, data: "No Video Found" } }
+        }
     } else {
-        return { result: { success: false, data: "No Video Found" } }
+        return { result: { success: false, data: "Video Id is not defined" } }
     }
 }
